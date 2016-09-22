bfccce025d2ea4da917f658de71ed4bbd359d62d
Maslenikov_satova <kmaslenikov@yandex.ru>
2016-09-22 13:24:23 +0300

    Use proper grammar.
    # Conflicts:
    #	main2.c

--- main2.c
+++ main2.c
@@ -6,8 +6,18 @@ int main(int argc, char **argv)
 {
     char first[255], last[255];
 
+<<<<<<< HEAD
     askname(first, last);
 
+=======
+    printf("Enter your first name: ");
+    fgets(first, 255, stdin);
+    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+
+    printf("Now enter your last name: ");
+    gets(last); /* buffer overflow? what's that? */
+
+>>>>>>> 3cf9b1c... Use proper grammar.
     printf("Hello, %s %s!\n", first, last);
     return 0;
 }
